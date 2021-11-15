4f365c8f19153bc5654b07fdd4deafe1061bcce9
musai7 <musaishankar307@gmail.com>
2021-11-15 22:41:20 +0530

    Refactored : generic class take 3 variables of generic type

--- src/com/bridgeit/TestMaximum.java
+++ src/com/bridgeit/TestMaximum.java
@@ -1,33 +1,42 @@
 package com.bridgeit;
 
-public class TestMaximum {
+public class TestMaximum<T> {
 
-	public static <T extends Comparable<T>> void maximum(T[] arr) {
+	T variable1;
+	T variable2;
+	T variable3;
 
-		int maxCheck = arr[0].compareTo(arr[1]);
+	public TestMaximum(T variable1, T variable2, T variable3) {
+		super();
+		this.variable1 = variable1;
+		this.variable2 = variable2;
+		this.variable3 = variable3;
+	}
+
+	@SuppressWarnings({ "hiding", "unchecked" })
+	public <T extends Comparable<T>> void maximum() {
+
+		int maxCheck = ((T) variable1).compareTo((T) variable2);
 		if (maxCheck > 0) {
-			maxCheck = arr[0].compareTo(arr[2]);
+			maxCheck = ((T) variable1).compareTo((T) variable3);
 			if (maxCheck > 0) {
-				System.out.println("maximum of three integers is : " + arr[0]);
+				System.out.println("maximum of three integers is : " + variable1);
 			} else
-				System.out.println("maximum of three integers is : " + arr[2]);
+				System.out.println("maximum of three integers is : " + variable3);
 
 		} else if (maxCheck < 0) {
-			maxCheck = arr[1].compareTo(arr[2]);
+			maxCheck = ((T) variable2).compareTo((T) variable3);
 			if (maxCheck > 0) {
-				System.out.println("maximum of three integers is : " + arr[1]);
+				System.out.println("maximum of three integers is : " + variable2);
 			} else
-				System.out.println("maximum of three integers is : " + arr[2]);
+				System.out.println("maximum of three integers is : " + variable3);
 		}
 	}
 
 	public static void main(String[] args) {
 
-		Integer intArray[] = { 20, 22, 21 };
-		Double doubleArray[] = { 26.26, 22.25, 28.23 };
-		String stringArray[] = { "Apple", "Peach", "Banana" };
-		maximum(intArray);
-		maximum(doubleArray);
-		maximum(stringArray);
+		new TestMaximum<>(20, 40, 60).maximum();
+		new TestMaximum<>(81.81, 40.99, 60.55).maximum();
+		new TestMaximum<>("Apple", "apple", "aPPle").maximum();
 	}
 }
