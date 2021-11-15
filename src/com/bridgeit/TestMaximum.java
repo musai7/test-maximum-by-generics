f7f261197da1d06161a394d573722f626fb58e2f
musai7 <musaishankar307@gmail.com>
2021-11-15 20:35:26 +0530

    added : maximum of 3 strings logic

--- src/com/bridgeit/TestMaximum.java
+++ src/com/bridgeit/TestMaximum.java
@@ -45,11 +45,33 @@ public class TestMaximum {
 		}
 	}
 	
+	private static void maximumSting(String[] stringArray) {
+		int maxCheck = stringArray[0].compareTo(stringArray[1]);
+		if(maxCheck>0) {
+			maxCheck=stringArray[0].compareTo(stringArray[2]);
+			if(maxCheck>0) {
+				System.out.println("maximum of three integers is : " + stringArray[0]);
+			}
+			else
+				System.out.println("maximum of three integers is : " + stringArray[2]);
+			
+		}else if(maxCheck<0) {
+			maxCheck=stringArray[1].compareTo(stringArray[2]); 
+			if(maxCheck>0) {
+				System.out.println("maximum of three integers is : " + stringArray[1]);
+			}
+			else
+				System.out.println("maximum of three integers is : " + stringArray[2]);
+		}	
+	}
+	
 	public static void main(String[] args) {
 		
 		Integer intArray[] = {20,22,21};
 		Double doubleArray[] = {26.26,22.25,28.23};
+		String stringArray[] = {"Apple","Peach","Banana"};
 		maximumInteger(intArray);
 		maximumDouble(doubleArray);
+		maximumSting(stringArray);
 	}
 }
