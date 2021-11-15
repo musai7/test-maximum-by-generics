4ec3375d4cc865c4f54d2a8f2da18ebb0bf8c04a
musai7 <musaishankar307@gmail.com>
2021-11-15 20:49:09 +0530

    refactor : maximum of three into a generic method

--- src/com/bridgeit/TestMaximum.java
+++ src/com/bridgeit/TestMaximum.java
@@ -2,76 +2,35 @@ package com.bridgeit;
 
 public class TestMaximum {
 	
-	public static void maximumInteger(Integer []intArr) {
+	public static <T extends Comparable<T>>void maximum(T []arr) {
 		
-		int maxCheck = intArr[0].compareTo(intArr[1]);
+		int maxCheck = arr[0].compareTo(arr[1]);
 		if(maxCheck>0) {
-			maxCheck=intArr[0].compareTo(intArr[2]);
+			maxCheck=arr[0].compareTo(arr[2]);
 			if(maxCheck>0) {
-				System.out.println("maximum of three integers is : " + intArr[0]);
+				System.out.println("maximum of three integers is : " + arr[0]);
 			}
 			else
-				System.out.println("maximum of three integers is : " + intArr[2]);
+				System.out.println("maximum of three integers is : " + arr[2]);
 			
 		}else if(maxCheck<0) {
-			maxCheck=intArr[1].compareTo(intArr[2]); 
+			maxCheck=arr[1].compareTo(arr[2]); 
 			if(maxCheck>0) {
-				System.out.println("maximum of three integers is : " + intArr[1]);
+				System.out.println("maximum of three integers is : " + arr[1]);
 			}
 			else
-				System.out.println("maximum of three integers is : " + intArr[2]);
+				System.out.println("maximum of three integers is : " + arr[2]);
 		}
 		
 	}
 	
-	public static void maximumDouble(Double []intArr) {
-		
-		int maxCheck = intArr[0].compareTo(intArr[1]);
-		if(maxCheck>0) {
-			maxCheck=intArr[0].compareTo(intArr[2]);
-			if(maxCheck>0) {
-				System.out.println("maximum of three integers is : " + intArr[0]);
-			}
-			else
-				System.out.println("maximum of three integers is : " + intArr[2]);
-			
-		}else if(maxCheck<0) {
-			maxCheck=intArr[1].compareTo(intArr[2]); 
-			if(maxCheck>0) {
-				System.out.println("maximum of three integers is : " + intArr[1]);
-			}
-			else
-				System.out.println("maximum of three integers is : " + intArr[2]);
-		}
-	}
-	
-	private static void maximumSting(String[] stringArray) {
-		int maxCheck = stringArray[0].compareTo(stringArray[1]);
-		if(maxCheck>0) {
-			maxCheck=stringArray[0].compareTo(stringArray[2]);
-			if(maxCheck>0) {
-				System.out.println("maximum of three integers is : " + stringArray[0]);
-			}
-			else
-				System.out.println("maximum of three integers is : " + stringArray[2]);
-			
-		}else if(maxCheck<0) {
-			maxCheck=stringArray[1].compareTo(stringArray[2]); 
-			if(maxCheck>0) {
-				System.out.println("maximum of three integers is : " + stringArray[1]);
-			}
-			else
-				System.out.println("maximum of three integers is : " + stringArray[2]);
-		}	
-	}
-	
-	public static void main(String[] args) {
+	public static  void main(String[] args) {
 		
 		Integer intArray[] = {20,22,21};
 		Double doubleArray[] = {26.26,22.25,28.23};
 		String stringArray[] = {"Apple","Peach","Banana"};
-		maximumInteger(intArray);
-		maximumDouble(doubleArray);
-		maximumSting(stringArray);
+		maximum(intArray);
+		maximum(doubleArray);
+		maximum(stringArray);
 	}
 }
